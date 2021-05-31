68be1af2e43bd7ceac564a7868f06c8ba0f1cf36
osj <ojsanch@gmail.com>
2021-05-31 18:40:57 -0500

    navbar routing working

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
 	return (
 		<div className="mx-auto lg:h-full pl-10 bg-gray-100 text-gray-500 text-xs text-center tracking-widest font-black">
-			<footer className="fixed bottom-0 w-full">© 2021 Oscar Sanchez Jr.</footer>
+			<footer className="w-full text-center border-t border-grey p-4 pin-b">© 2021 Oscar Sanchez Jr.</footer>
 		</div>
 	);
 };
---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import IntroImage from '../IntroImage/IntroImage';
+import About from '../About/About';
+import Contact from '../Contact/Contact';
+
+const Home = () => {
+	return (
+		<div>
+			<IntroImage />
+			<About />
+			<Contact />
+		</div>
+	);
+};
+
+export default Home;
